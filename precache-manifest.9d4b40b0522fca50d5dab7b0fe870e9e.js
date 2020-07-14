dfc8f963d0362232fca806d65f2bae80c5c8fdd7
Taisiia Pomazan <taisiia.pomazan@gmail.com>
2020-07-14 17:07:26 +0300

    Updates

---
+++ precache-manifest.9d4b40b0522fca50d5dab7b0fe870e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffea51b4e061451ba061f9da6138937",
+    "url": "/todo_list/index.html"
+  },
+  {
+    "revision": "04325a80c557706efc5b",
+    "url": "/todo_list/static/css/main.581925f1.chunk.css"
+  },
+  {
+    "revision": "f75d59a926a81a2ba042",
+    "url": "/todo_list/static/js/2.6ed89c82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list/static/js/2.6ed89c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04325a80c557706efc5b",
+    "url": "/todo_list/static/js/main.c2ca94ba.chunk.js"
+  },
+  {
+    "revision": "cf74516e357f9a0d23f8",
+    "url": "/todo_list/static/js/runtime-main.7a3e04cd.js"
+  }
+]);
